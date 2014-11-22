dd9731e9693896f97fa296ca275373cd79918c7b
Natali Toupin <natalitoupin@gmail.com>
2014-11-22 14:55:52 -0500

    Set up index view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,50 @@
 class BooksController < ApplicationController
-  
+
+	def index
+		@books = params[:q] ? Books.search_for(params[:q]) : Books.all
+	end
+
+	def show
+		@book = Books.find params[:id]
+		@omment = Comment.new
+	end
 end
+
+# 	def new
+# 		@book = Book.new
+# 	end
+
+  
+# end
+# class StoriesController < ApplicationController
+#   before_action :authenticate_user!, only: [:new, :create]
+
+#   def index
+#     @stories = params[:q] ? Story.search_for(params[:q]) : Story.all
+#   end
+
+#   def hot
+#     @stories = Story.popular
+#   end
+
+#   def show
+#     @story = Story.find params[:id]
+#     @comment = Comment.new
+#   end
+
+#   def new
+#     @story = Story.new
+#   end
+
+#   def create
+#     safe_story_params = params.require(:story).permit(:title, :link, :category).merge(user_id: current_user.id)
+#     @story = Story.new safe_story_params.merge(:upvotes => 1)
+#     #@story.user = current_user
+
+#     if @story.save
+#       redirect_to @story
+#     else
+#       render :new
+#     end
+#   end
+# end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,8 +32,7 @@ Rails.application.configure do
   # Generate digests for assets URLs.
   config.assets.digest = true
 
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
+  # `config.assets.precompile` and `config.assets.version` have moved to conf
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
